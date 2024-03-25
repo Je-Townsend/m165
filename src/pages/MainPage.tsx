a89f9c812ae6cdc5eaadc06969ae2cf384b2bd5d
Yasin Kücüktas <yku140671@stud.gibb.ch>
2024-03-25 17:17:25 +0100

    added api calls

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,48 +1,49 @@
 import BandSong from "../Components/BandSong";
 import "../App.css";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 
-export default function QuizOverview() {
-  //useEffect(() => {
-  //  const res = async () => {
-  //    await axios.get("http://localhost:8000");
-  //  };
-  //  res();
-  //}, []);
+export default function MainPage() {
+  const [responseShow, setResponseShow] = useState<any>(null); // Verwende useState, um responseShow zu speichern
 
-  const BandName = "Example Band";
-  const BandGenre = "Rock";
-  const title = "Example Song";
-  const band_id = 1;
-  const duration = 255;
-  const composer = "John Doe";
-  const producer = "Jane Doe";
-  const album = "Example Album";
-  const spotify_link = "https://example.com";
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://localhost:8080/songs');
+        setResponseShow(response.data[0]); // Setze die Antwortdaten in den responseShow-State
+        console.log('Daten erhalten:', response.data[1]);
+      } catch (error) {
+        console.error('Fehler beim Abrufen der Daten:', error);
+      }
+    };
+    
+    fetchData();
+  }, []);
 
   function secondsToMinutes(secs: number) {
     let minutenGanz = secs / 60;
     var mins = Math.floor(minutenGanz);
     var restlicheSekunden = secs - mins * 60;
     return mins + " Minuten " + restlicheSekunden + " Sekunden";
-}
+  }
 
   return (
     <div className="Band">
       <h1>MainPage</h1>
       <br />
-      <BandSong
-        BandName={BandName}
-        BandGenre={BandGenre}
-        title={title}
-        band_id={band_id}
-        duration={secondsToMinutes(duration)}
-        composer={composer}
-        producer={producer}
-        album={album}
-        spotify_link={spotify_link}
-      />
+      {responseShow && ( // Zeige BandSong nur an, wenn responseShow definiert ist
+        <BandSong
+          BandName={responseShow.band.name}
+          BandGenre={responseShow.band.genre}
+          title={responseShow.title}
+          band_id={responseShow.band.id}
+          duration={secondsToMinutes(responseShow.duration)}
+          composer={responseShow.composer}
+          producer={responseShow.producer}
+          album={responseShow.album}
+          spotify_link={responseShow.spotify_link}
+        />
+      )}
     </div>
   );
 }
